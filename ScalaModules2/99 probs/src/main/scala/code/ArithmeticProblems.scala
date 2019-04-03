3be8557a4147cce3174cfd8abdf1f89e3909c050
Rhys <rjones.zx@gmail.com>
2019-04-03 17:36:46 +0100

    Updated with comments and sbt file structure

--- ScalaModules2/99 probs/src/main/scala/code/ArithmeticProblems.scala
+++ ScalaModules2/99 probs/src/main/scala/code/ArithmeticProblems.scala
@@ -3,16 +3,15 @@ package code
 class S99Int(val start: Int) {
   import S99Int._
 // P31
-//  def isPrime(n: Int) = ( if(n < 2) false
-//    else (2 to math.sqrt(n).toInt) forall (n % _ != 0) )
-
   def isPrime: Boolean =
     (start > 1) && (primes takeWhile { _ <= math.sqrt(start) } forall { start % _ != 0 })
 
+// P32: see companion object
+// P33
   def isCoprimeTo(n: Int) = gcd(start, n) == 1
-
+// P34
   def totient = (1 to start).filter(_.isCoprimeTo(start)).length
-
+// P35
   def primeFactors: List[Int] = {
     val it = primes.toIterator
     def get(n: Int, p:Int, lst: List[Int]): List[Int] = n match {
@@ -22,7 +21,7 @@ class S99Int(val start: Int) {
     }
     get(start, it.next(), List[Int]()).reverse
   }
-
+// P36
   def primeFactorMultiplicity = {
     primeFactors.foldRight (List[(Int, Int)]()) ((a, b) => (a, b) match {
       case (_, Nil)                   => (a, 1) :: b
@@ -33,11 +32,14 @@ class S99Int(val start: Int) {
 
   def pfm2 = primeFactors.distinct map{i => (i, primeFactors.filter(_ == i).length)}
 
+// P37
   def phi = {
     val p = primeFactorMultiplicity
     (1 /: p)((a, b) => a * (b._1 - 1) * math.pow(b._1, (b._2 - 1)).toInt)
   }
-
+// P38: TODO
+// P39: see companion object
+// P40
   def goldbach:(Int, Int) = {
     if ((start > 2) && (start % 2 == 0)) {
       val p1 = primes.dropWhile( i => !(start - i).isPrime ).head
@@ -52,6 +54,8 @@ class S99Int(val start: Int) {
       case None     => throw new IllegalArgumentException
       case Some(p1) => (p1, start - p1)
   }
+
+// P41 TODO
 }
 
 object S99Int {
@@ -59,11 +63,12 @@ object S99Int {
 
   val primes = Stream.cons(2, Stream.from(3, 2) filter { _.isPrime })
 
+// P32
   def gcd(a: Int, b: Int): Int = b match {
     case 0 => a
     case _ => gcd(b, a % b)
   }
-
+// P39
   def listPrimesinRange (r: Range) = {
     primes.dropWhile(_ < r.head).takeWhile(_ <= r.last).toList
   }
